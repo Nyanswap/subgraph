802f229967f9dc6ccf76a87ee7614b7e0e0f50f0
catnip-nyanswap <catnip@nyanswap.com>
2020-10-23 14:17:29 +0900

    change address

--- src/mappings/helpers.ts
+++ src/mappings/helpers.ts
@@ -7,7 +7,7 @@ import { User, Bundle, Token, LiquidityPosition, LiquidityPositionSnapshot, Pair
 import { Factory as FactoryContract } from '../types/templates/Pair/Factory'
 
 export const ADDRESS_ZERO = '0x0000000000000000000000000000000000000000'
-export const FACTORY_ADDRESS = '0xc2654aD857f85D5c681b3Ab9Ec4E834c3E4D5415'
+export const FACTORY_ADDRESS = '0x99760F6d4D19cF41258c2B174F110786c55F9b5E'
 
 export let ZERO_BI = BigInt.fromI32(0)
 export let ONE_BI = BigInt.fromI32(1)
